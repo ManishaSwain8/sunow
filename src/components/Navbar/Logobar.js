5d0f81b36107acf5471f066982a595a04a6cd820
Manisha Swain <manishaswain2003@gmail.com>
2022-07-17 17:14:18 +0530

    Added responsiveness to navbar

--- src/components/Navbar/Logobar.js
+++ src/components/Navbar/Logobar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import image from "../../assets/images/logo.svg";
 export default function Logobar(){
     return(
     <header className="logobar">
-        <img src="./imagespub/logo.png" className="logo" alt="error"></img>
-        <h2 className="logoname">Sunow</h2>
+        <img src={image} className="logo" alt="error"></img>
+        <h2 className="logoname"><b>Sunow</b></h2>
         
     </header>)
 }
