0335fa79cb3d50dc08a1a090df71be1b31acdd65
Manisha Swain <manishaswain2003@gmail.com>
2022-08-16 23:19:45 +0530

    added search bar drop box

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Explore.css";
 import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from "@mui/icons-material/Close";
+
 export default function Search() {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="search mx-auto">
-      <input className="text_bar" type="text" placeholder="Artists , songs , or podcasts"></input>
+      <input
+        onClick={() => setShow(true)}
+        className="text_bar"
+        type="text"
+        placeholder="Dive in!"
+      ></input>
 
       <div className="button_search">
         <SearchIcon size={30} />
       </div>
+      <button className="close" onClick={() => setShow(false)}>
+        <CloseIcon size={30} />
+      </button>
+
+      {show && (
+        <div className="result">
+          <input
+            className="text_bar1"
+            type="text"
+            placeholder="Dive in!"
+          ></input>
+          <div className="button_search1">
+            <SearchIcon size={30} />
+          </div>
+          <button className="close1" onClick={() => setShow(false)}>
+            <CloseIcon size={30} />
+          </button>
+          <h2 className="font-bold text-2lg ml-10 mt-10">TRENDING</h2>
+          <div>
+            <Top50Box />
+            <Top50Box />
+            <Top50Box />
+            <Top50Box />
+            <Top50Box />
+            <Top50Box />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
+const Top50Box = () => {
+  return (
+    <div className=" hover:shadow  inline-block ml-9 mt-6 space-x-5 ">
+      <div>
+        <img
+          src="http://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg"
+          alt="albumImage"
+          className="rounded-md w-12"
+        />
+        <h2>Mosaic Cool </h2>
+        <h3>Engineering karke galti kar die</h3>
+      </div>
+    </div>
+  );
+};
--- src/components/Home/RecentPlay.jsx
+++ src/components/Home/RecentPlay.jsx
@@ -5,17 +5,22 @@ import { GiFastForwardButton } from "react-icons/gi";
 import { GiPreviousButton } from "react-icons/gi";
 import { GiNextButton } from "react-icons/gi";
 
-export default function RecentPlay() {
+/*export default function RecentPlay() {
   return (
     <div className=" self-stretch  max-h-[500px] md:block hidden relative">
       <div className="bg-[#e2e2f2] mx-auto max-w-[400px] h-full shadow-xl rounded-2xl p-6 text-center">
         <div className="flex flex-col h-full justify-around">
           <div className="max-w-xs mx-auto">
-            <img src="https://i1.sndcdn.com/artworks-3ITTy648eljqFP3e-LGDfnQ-t500x500.jpg" alt="heatwave" className="mx-auto rounded-2xl" width={250} />
-            {/* <img src="https://upload.wikimedia.org/wikipedia/en/b/b0/Glass_Animals_-_Heat_Waves.png" alt="heatwave" className="mx-auto rounded-2xl w-full" width={300} /> */}
-          </div>
-          {/* <div className="mt-2"> */}
-          <div>
+            <img
+              src="https://i1.sndcdn.com/artworks-3ITTy648eljqFP3e-LGDfnQ-t500x500.jpg"
+              alt="heatwave"
+              className="mx-auto rounded-2xl"
+              width={250}
+            />
+            {/* <img src="https://upload.wikimedia.org/wikipedia/en/b/b0/Glass_Animals_-_Heat_Waves.png" alt="heatwave" className="mx-auto rounded-2xl w-full" width={300} /> */
+/*</div>
+          {/* <div className="mt-2"> */
+/*<div>
             <h1>Heat Waves</h1>
             <h4>glass animals</h4>
           </div>
@@ -31,10 +36,35 @@ export default function RecentPlay() {
           <div className="flex items-center justify-evenly mt-2">
             <GiPreviousButton size={20} color="rgb(251 146 60)" />
             <GiFastBackwardButton size={20} color="rgb(251 146 60)" />
-            <BsFillPlayCircleFill size={40} color="rgb(251 146 60)" className="shadow-2xl shadow-slate-600" />
+            <BsFillPlayCircleFill
+              size={40}
+              color="rgb(251 146 60)"
+              className="shadow-2xl shadow-slate-600"
+            />
             <GiFastForwardButton size={20} color="rgb(251 146 60)" />
             <GiNextButton size={20} color="rgb(251 146 60)" />
           </div>
+        </div>
+      </div>
+    </div>
+  );
+}*/
+export default function RecentPlay() {
+  return (
+    <div class="super_container">
+      <div class="container">
+        <iframe
+          src="https://open.spotify.com/embed?uri=spotify:track:2kxQjpmDPCFX2RWfMIuYDi"
+          width="400"
+          height="400"
+          frameborder="0"
+          allow="encrypted-media"
+        ></iframe>
+        <div class="box left">
+          <i class="fa-solid fa-backward-step"></i>
+        </div>
+        <div class="box right">
+          <i class="fa-solid fa-forward-step"></i>
         </div>
       </div>
     </div>
