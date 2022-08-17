32281325f19341563be7d51e56636b4f28f09788
Krishna <nayak100krishna@gmail.com>
2022-08-17 07:36:24 +0530

    tries to make responsive

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState } from "react";
 import "./Explore.css";
 import SearchIcon from "@mui/icons-material/Search";
@@ -8,34 +9,26 @@ export default function Search() {
 
   return (
     <div className="search mx-auto">
-      <input
-        onClick={() => setShow(true)}
-        className="text_bar"
-        type="text"
-        placeholder="Dive in!"
-      ></input>
-
-      <div className="button_search">
-        <SearchIcon size={30} />
+      <div className="relative text-center">
+        <input onClick={() => setShow(true)} className="text_bar" type="text" placeholder="Dive in!" />
+        <div className="button_search">
+          <SearchIcon size={30} />
+        </div>
+        <button className="close" onClick={() => setShow(false)}>
+          <CloseIcon size={30} />
+        </button>
       </div>
-      <button className="close" onClick={() => setShow(false)}>
-        <CloseIcon size={30} />
-      </button>
 
-      {show && (
+      {/* {show && (
         <div className="result">
-          <input
-            className="text_bar1"
-            type="text"
-            placeholder="Dive in!"
-          ></input>
+          <input className="text_bar1" type="text" placeholder="Dive in!"></input>
           <div className="button_search1">
             <SearchIcon size={30} />
           </div>
           <button className="close1" onClick={() => setShow(false)}>
             <CloseIcon size={30} />
           </button>
-          <h2 className="font-bold text-2lg ml-10 mt-10">TRENDING</h2>
+          <h2 className="font-bold text-2xl ml-10 mt-10">TRENDING</h2>
           <div>
             <Top50Box />
             <Top50Box />
@@ -45,7 +38,7 @@ export default function Search() {
             <Top50Box />
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
@@ -53,11 +46,7 @@ const Top50Box = () => {
   return (
     <div className=" hover:shadow  inline-block ml-9 mt-6 space-x-5 ">
       <div>
-        <img
-          src="http://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg"
-          alt="albumImage"
-          className="rounded-md w-12"
-        />
+        <img src="http://filmmusicreporter.com/wp-content/uploads/2018/05/mosaic.jpg" alt="albumImage" className="rounded-md w-12" />
         <h2>Mosaic Cool </h2>
         <h3>Engineering karke galti kar die</h3>
       </div>
